be850ab112643e7c27e04158bb75ecf1103f77f7
Holtz Yan <yan.holtz.data@gmail.com>
2018-12-19 11:52:32 +1000

    skeleton is ok. ready to work

---
+++ DATA/data_preparation.R
@@ -0,0 +1,16 @@
+####
+# Data Preparation
+#
+# This R script reads data sent by Oleguer and create all the inputs for the d3.js charts
+#
+# Run it with --> Rscript data_preparation.R
+###
+
+
+
+# Step 1: Read input files
+
+
+# Data: symmetric stacked barplot
+
+# Save to JSON! 
