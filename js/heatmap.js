6b5599da7db7d8ddb5b36d858db9d995c2334acd
Holtz Yan <yan1166@hotmail.com>
2019-01-31 15:07:04 +1000

    Heat map gets better

--- js/heatmap.js
+++ js/heatmap.js
@@ -1,7 +1,15 @@
+function plotHeatmap(){
+
+
+
+// ======================= //
+// DATA, SVG AREAS
+// ======================= //
+
 // set the dimensions and margins of the graph
-var margin = {top: 30, right: 30, bottom: 120, left: 120},
-  width = 700 - margin.left - margin.right,
-  height = 650 - margin.top - margin.bottom;
+var margin = {top: 30, right: 30, bottom: 130, left: 120},
+  width = 600 - margin.left - margin.right,
+  height = 600 - margin.top - margin.bottom;
 
 // append the svg object to the body of the page
 var svg = d3.select("#my_heatmap")
@@ -16,31 +24,50 @@ var svg = d3.select("#my_heatmap")
 var myGroups = allCOD
 var myVars = allDisorder
 
+
+
+// ======================= //
+// AXIS and SCALES
+// ======================= //
+
 // Build X scales and axis:
 var x = d3.scaleBand()
   .range([ 0, width ])
   .domain(myGroups)
   .padding(0.01);
-svg.append("g")
+var xAxis = svg.append("g")
   .attr("transform", "translate(0," + height + ")")
-  .call(d3.axisBottom(x))
-  .selectAll("text")
+  .call(d3.axisBottom(x).tickSize(0))
+xAxis.selectAll("text")
     .attr("transform", "translate(-10,10)rotate(-45)")
     .style("text-anchor", "end")
-
+    .style("font-size", "12px")
+xAxis.select(".domain").remove()
 
 // Build X scales and axis:
 var y = d3.scaleBand()
   .range([ height, 0 ])
   .domain(myVars)
   .padding(0.01);
-svg.append("g")
-  .call(d3.axisLeft(y));
+var yAxis = svg.append("g")
+  .call(d3.axisLeft(y).tickSize(0))
+yAxis.selectAll("text")
+  .style("font-size", "12px")
+yAxis.select(".domain").remove();
+
 
 // Build color scale
 var myColor = d3.scaleLinear()
-  .range(["white", "#69b3a2"])
+  .range(["white", "#ED3A3B"])
   .domain([1,10])
+
+
+
+
+
+// ======================= //
+// SHAPES
+// ======================= //
 
 // Add squares
 svg.selectAll()
@@ -52,3 +79,8 @@ svg.selectAll()
     .attr("width", x.bandwidth() )
     .attr("height", y.bandwidth() )
     .style("fill", function(d) { return myColor(d.MRR)} )
+
+}
+
+
+plotHeatmap()
