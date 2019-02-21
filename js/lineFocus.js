494c491c36deeba4a21475d764a02b7ffadbeef1
Holtz Yan <yan1166@hotmail.com>
2019-02-21 10:21:45 +0100

    Line plot split in 2 parts + age available on hover (finally)

--- js/lineFocus.js
+++ js/lineFocus.js
@@ -1,4 +1,4 @@
-function plotLine(){
+function plotLineFocus(){
 
 
 // ======================= //
@@ -7,6 +7,8 @@ function plotLine(){
 
 // Get filtered data
 data_allGrouped = data_MRRage.filter(function(d){ return d.dx2 == "Any Disorder" & d.cod_label == "All Causes" & d.sex2 == "Persons" & d.specific > 15})
+data_sexSpecific = data_MRRage.filter(function(d){ return d.dx2 == "Any Disorder" & d.cod_label == "All Causes" & (d.sex2 == "Males" || d.sex2 == "Females") & d.specific > 15})
+data_codSpecific = data_MRRage.filter(function(d){ return d.dx2 == "Any Disorder" & d.cod_label != "All Causes" & d.sex2 == "Persons" & d.specific > 15})
 
 // set the dimensions and margins of the graph
 var margin = {top: 10, right: 30, bottom: 50, left: 60},
@@ -14,7 +16,7 @@ var margin = {top: 10, right: 30, bottom: 50, left: 60},
     height = 440 - margin.top - margin.bottom;
 
 // append the svg object to the body of the page
-var svgLeft = d3.select("#my_MR")
+var svgLeft = d3.select("#my_MR_focus")
   .append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
@@ -23,7 +25,7 @@ var svgLeft = d3.select("#my_MR")
           "translate(" + margin.left + "," + margin.top + ")");
 
 // append the svg object to the body of the page
-var svgRight = d3.select("#my_MRR")
+var svgRight = d3.select("#my_MRR_focus")
   .append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
@@ -34,9 +36,6 @@ var svgRight = d3.select("#my_MRR")
 var mySexColor = d3.scaleOrdinal()
   .domain(["Persons", "Males", "Females"])
   .range(["steelblue", "#1E8F89", "#EE5A45"])
-
-
-
 
 
 // ======================= //
@@ -296,151 +295,135 @@ function updateChart(data){
       .remove()
 
 
+// This allows to find the closest X index of the mouse:
+var bisect = d3.bisector(function(d) { return d.specific; }).left;
 
-
-
-
-  // ======================= //
-  // ANNOTATION ON HOVER
-  // ======================= //
-
-  // This allows to find the closest X index of the mouse:
-  var bisect = d3.bisector(function(d) { return d.specific; }).left;
-
-  // Circle that travels along the curve of right chart
-  var focusRight = svgRight.append('g')
-    .append('circle')
-      .style("fill", "none")
-      .attr("stroke", "black")
-      .attr('r', 8.5)
-      .style("opacity", 0)
-
-  // Text along the curve of right chart
-  var textRight = svgRight.append('g')
-    .append('text')
-      .style("fill", "black")
-      .style("opacity", 0)
-      .attr("text-anchor", "middle")
-
-  // Circle and text for left part
-  var focusLeftTop = svgLeft.append('g')
-    .append('circle')
-      .style("fill", "black")
-      .attr('r', 3.5)
-      .style("opacity", 0)
-  var textLeftTop = svgLeft.append('g')
-    .append('text')
-      .style("fill", "black")
-      .style("opacity", 0)
-      .attr("text-anchor", "end")
-  var focusLeftBottom = svgLeft.append('g')
-    .append('circle')
-      .style("fill", "black")
-      .attr('r', 3.5)
-      .style("opacity", 0)
-  var textLeftBottom = svgLeft.append('g')
-    .append('text')
-      .style("fill", "black")
-      .style("opacity", 0)
-      .attr("text-anchor", "start")
-
-  // and the age
-  var textLeftAge = svgLeft.append('g')
-    .append('text')
-      .style("fill", "black")
-      .style("opacity", 0)
-      .attr("text-anchor", "middle")
-
-  // Create the Line that travels along both curves of left chart
-  var focusLeftLine = svgLeft.append('g')
-    .append('line')
-      .style("stroke", "black")
-      .style("opacity", 0)
-
-  // Create 2 rect on top of each of the svg areas: this rectangle recovers mouse position
-  svgRight.append('rect')
+// Create the circle that travels along the curve of right chart
+var focusRight = svgRight.append('g')
+  .append('circle')
     .style("fill", "none")
-    .style("pointer-events", "all")
-    .attr('width', width)
-    .attr('height', height)
-    .on('mouseover', mouseover)
-    .on('mousemove', mousemove)
-    .on('mouseout', mouseout);
+    .attr("stroke", "black")
+    .attr('r', 8.5)
+    .style("opacity", 0)
 
-  svgLeft.append('rect')
-    .style("fill", "none")
-    .style("pointer-events", "all")
-    .attr('width', width)
-    .attr('height', height)
-    .on('mouseover', mouseover)
-    .on('mousemove', mousemove)
-    .on('mouseout', mouseout);
+// Create the text along the curve of left chart
+var textRight = svgRight.append('g')
+  .append('text')
+    .style("fill", "black")
+    .style("opacity", 0)
+    .attr("text-anchor", "middle")
 
-  // What happens when the mouse move -> show the annotations at the right positions.
-  function mouseover() {
-    focusRight.style("opacity", 1)
-    focusLeftTop.style("opacity", 1)
-    focusLeftBottom.style("opacity", 1)
-    focusLeftLine.style("opacity", 1)
-    textLeftTop.style("opacity", 1)
-    textLeftBottom.style("opacity", 1)
-    textRight.style("opacity",1)
-    textLeftAge.style("opacity",1)
-  }
+// Create the circle that travels along the curve of left chart
+var focusLeftTop = svgLeft.append('g')
+  .append('circle')
+    .style("fill", "black")
+    .attr('r', 3.5)
+    .style("opacity", 0)
 
-  function mousemove() {
-    // recover coordinate we need
-    var x0 = x.invert(d3.mouse(this)[0]);
-    var i = bisect(data, x0, 1);
-    selectedData = data[i]
-    focusRight
-      .attr("cx", x(selectedData.specific))
-      .attr("cy", yRight(selectedData.irr))
-    textRight
-      .text(Math.round(selectedData.irr)+" %")
-      .attr("x", function(){ if(selectedData.specific<35){
-          return x(selectedData.specific)-25
-        }else{
-          return x(selectedData.specific)+25
-      }})
-      .attr("y", yRight(selectedData.irr))
-    focusLeftTop
-      .attr("cx", x(selectedData.specific))
-      .attr("cy", yLeft(selectedData.undiagnosed))
-    focusLeftBottom
-      .attr("cx", x(selectedData.specific))
-      .attr("cy", yLeft(selectedData.diagnosed))
-    focusLeftLine
-      .attr("x1", x(selectedData.specific))
-      .attr("x2", x(selectedData.specific))
-      .attr("y1", yLeft(selectedData.undiagnosed))
-      .attr("y2", yLeft(selectedData.diagnosed))
-    textLeftTop
-      .text(Math.round(selectedData.diagnosed))
-      .attr("x",x(selectedData.specific)-10)
-      .attr("y", yLeft(selectedData.diagnosed))
-    textLeftBottom
-      .text(Math.round(selectedData.undiagnosed))
-      .attr("x",x(selectedData.specific)+10)
-      .attr("y", yLeft(selectedData.undiagnosed))
-    textLeftAge
-      .text("Age: " + selectedData.specific)
-      .attr("x",x(selectedData.specific)+10)
-      .attr("y", yLeft(selectedData.undiagnosed)+30 )
-  }
-  function mouseout() {
-    focusRight.style("opacity", 0)
-    focusLeftTop.style("opacity", 0)
-    focusLeftBottom.style("opacity", 0)
-    focusLeftLine.style("opacity", 0)
-    textLeftTop.style("opacity", 0)
-    textLeftBottom.style("opacity", 0)
-    textRight.style("opacity",0)
-    textLeftAge.style("opacity",0)
-  }
+// Create the text along the curve of left chart
+var textLeftTop = svgLeft.append('g')
+  .append('text')
+    .style("fill", "black")
+    .style("opacity", 0)
+    .attr("text-anchor", "end")
+
+// Create the circle that travels along the curve of left chart
+var focusLeftBottom = svgLeft.append('g')
+  .append('circle')
+    .style("fill", "black")
+    .attr('r', 3.5)
+    .style("opacity", 0)
+
+// Create the text along the curve of left chart
+var textLeftBottom = svgLeft.append('g')
+  .append('text')
+    .style("fill", "black")
+    .style("opacity", 0)
+    .attr("text-anchor", "start")
+
+// Create the Line that travels along both curves of left chart
+var focusLeftLine = svgLeft.append('g')
+  .append('line')
+    .style("stroke", "black")
+    .style("opacity", 0)
+
+// Create 2 rect on top of each of the svg areas: this rectangle recovers mouse position
+svgRight.append('rect')
+  .style("fill", "none")
+  .style("pointer-events", "all")
+  .attr('width', width)
+  .attr('height', height)
+  .on('mouseover', mouseover)
+  .on('mousemove', mousemove)
+  .on('mouseout', mouseout);
+
+svgLeft.append('rect')
+  .style("fill", "none")
+  .style("pointer-events", "all")
+  .attr('width', width)
+  .attr('height', height)
+  .on('mouseover', mouseover)
+  .on('mousemove', mousemove)
+  .on('mouseout', mouseout);
+
+// What happens when the mouse move -> show the annotations at the right positions.
+function mouseover() {
+  focusRight.style("opacity", 1)
+  focusLeftTop.style("opacity", 1)
+  focusLeftBottom.style("opacity", 1)
+  focusLeftLine.style("opacity", 1)
+  textLeftTop.style("opacity", 1)
+  textLeftBottom.style("opacity", 1)
+  textRight.style("opacity",1)
+}
+
+function mousemove() {
+  // recover coordinate we need
+  var x0 = x.invert(d3.mouse(this)[0]);
+  var i = bisect(data, x0, 1);
+  selectedData = data[i]
+  focusRight
+    .attr("cx", x(selectedData.specific))
+    .attr("cy", yRight(selectedData.irr))
+  textRight
+    .text(Math.round(selectedData.irr)+" %")
+    .attr("x", function(){ if(selectedData.specific<35){
+        return x(selectedData.specific)-25
+      }else{
+        return x(selectedData.specific)+25
+    }})
+    .attr("y", yRight(selectedData.irr))
+  focusLeftTop
+    .attr("cx", x(selectedData.specific))
+    .attr("cy", yLeft(selectedData.undiagnosed))
+  focusLeftBottom
+    .attr("cx", x(selectedData.specific))
+    .attr("cy", yLeft(selectedData.diagnosed))
+  focusLeftLine
+    .attr("x1", x(selectedData.specific))
+    .attr("x2", x(selectedData.specific))
+    .attr("y1", yLeft(selectedData.undiagnosed))
+    .attr("y2", yLeft(selectedData.diagnosed))
+  textLeftTop
+    .text(Math.round(selectedData.diagnosed))
+    .attr("x",x(selectedData.specific)-10)
+    .attr("y", yLeft(selectedData.diagnosed))
+  textLeftBottom
+    .text(Math.round(selectedData.undiagnosed))
+    .attr("x",x(selectedData.specific)+10)
+    .attr("y", yLeft(selectedData.undiagnosed))
+}
+function mouseout() {
+  focusRight.style("opacity", 0)
+  focusLeftTop.style("opacity", 0)
+  focusLeftBottom.style("opacity", 0)
+  focusLeftLine.style("opacity", 0)
+  textLeftTop.style("opacity", 0)
+  textLeftBottom.style("opacity", 0)
+  textRight.style("opacity",0)
+}
 
 
-// Close the updateChart function
 }
 
 // Initialise the chart
@@ -448,6 +431,37 @@ updateChart(data_allGrouped)
 
 
 
-// close plotLine function
+
+
+
+
+// ======================= //
+// ADD THE ANNOTATION WHEN HOVER
+// ======================= //
+
+
+
+
+
+
+// ======================= //
+// SHOW 2 LINES FOR SEX IF BUTTON IS CLICKED
+// ======================= //
+
+
+
+// If user clicks on something with the class  showSex.. -> show sex
+var anchors = document.getElementsByClassName("showSexLinechart")
+for(var i = 0; i < anchors.length; i++) {
+  anchors[i].onclick = function(){ updateChart(data_sexSpecific)}
 }
-plotLine()
+// And for grouping everything
+document.getElementById("showAllGrouped").onclick = function(){ updateChart(data_allGrouped)} ;
+
+
+
+
+
+
+}
+plotLineFocus()
